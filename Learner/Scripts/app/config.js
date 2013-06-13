c16997ec652f3c3dd9026ffcd367880e6947ffd7
blaster151 <blaster151@gmail.com>
2013-06-13 12:14:59 -0500

    Fix pathing for HTML templates

--- Learner/Scripts/app/config.js
+++ Learner/Scripts/app/config.js
@@ -4,29 +4,28 @@ app.config(function ($routeProvider) {
     $routeProvider.when(
         "/page1",
         {
-            templateUrl: "http://localhost:23253/scripts/app/page1.html"
+            templateUrl: "/scripts/app/page1.html"
         }
     ).when(
         "/page2",
         {
-            templateUrl: "http://localhost:23253/scripts/app/page2.html"
+            templateUrl: "/scripts/app/page2.html"
         }
     ).when(
         "/quiz",
         {
-            templateUrl: "http://localhost:23253/scripts/app/quiz.html",
+            templateUrl: "/scripts/app/quiz.html",
             controller: "QuizModeCtrl"
         }
     ).when(
         "/browse",
         {
-            templateUrl: "http://localhost:23253/scripts/app/browse.html",
+            templateUrl: "/scripts/app/browse.html",
             controller: "QuizCtrl"
         }
     ).otherwise(
         {
-            templateUrl: "http://localhost:23253/scripts/app/browse.html",
-            controller: "QuizCtrl"
-}
+            redirectTo: '/browse'
+        }
     );
 });
